d47622c3e7b5889bdc3890b97dcdb8cebb211c91
LeoHalfar <151862077+LeoHalfar@users.noreply.github.com>
2023-12-04 20:12:57 +0100

    Update task3q3.sql

--- task3q3.sql
+++ task3q3.sql
@@ -8,11 +8,11 @@ FROM
 JOIN
     lesson ON instructor.instructor_id = lesson.instructor_id
 WHERE
-    EXTRACT(MONTH FROM lesson.start_time) = 11
+    EXTRACT(MONTH FROM lesson.start_time) = 12
     AND EXTRACT(YEAR FROM lesson.start_time) = 2023
 GROUP BY
     instructor.instructor_id, instructor.first_name, instructor.last_name
 HAVING
-    COUNT(lesson.lesson_id) > 1
+    COUNT(lesson.lesson_id) > 2
 ORDER BY
     total_lessons DESC;
